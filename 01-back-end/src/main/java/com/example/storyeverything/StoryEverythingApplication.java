34d686dc3a6578780ccc74b9937892a32232ab17
Kacper Pietrzykowski <kacperpietrzykowski2000@gmail.com>
2024-03-17 22:56:42 +0100

    init

---
+++ 01-back-end/src/main/java/com/example/storyeverything/StoryEverythingApplication.java
@@ -0,0 +1,13 @@
+package com.example.storyeverything;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryEverythingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoryEverythingApplication.class, args);
+	}
+
+}
