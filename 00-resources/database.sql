7c2a04f5c0b6df90c148dc89b7359031cd6af319
Kacper Pietrzykowski <kacperpietrzykowski2000@gmail.com>
2024-03-24 19:57:43 +0100

    sql edit

--- 00-resources/database.sql
+++ 00-resources/database.sql
@@ -4,6 +4,12 @@ CREATE TABLE category (
     name VARCHAR(20) NOT NULL CHECK (length(name) BETWEEN 3 AND 20 AND lower(name) = name)
 );
 
+-- Tabela dla ról
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(20) NOT NULL UNIQUE CHECK (name IN ('ADMIN', 'LIMITED_USER', 'FULL_USER', 'GUEST'))
+);
+
 -- Tabela dla użytkowników
 CREATE TABLE user_account (
     id SERIAL PRIMARY KEY,
@@ -11,7 +17,9 @@ CREATE TABLE user_account (
     last_name VARCHAR(50) NOT NULL CHECK (length(last_name) BETWEEN 3 AND 50 AND last_name = initcap(last_name)),
     login VARCHAR(20) UNIQUE NOT NULL CHECK (length(login) BETWEEN 3 AND 20 AND lower(login) = login),
     password VARCHAR(255) NOT NULL CHECK (length(password) >= 5),
-    age INTEGER NOT NULL CHECK (age >= 18)
+    age INTEGER NOT NULL CHECK (age >= 18),
+    role_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES role(id)
 );
 
 -- Tabela dla informacji
@@ -22,5 +30,10 @@ CREATE TABLE information (
     link TEXT, -- Można dodać walidację URL, jeśli jest potrzebna
     add_date DATE NOT NULL DEFAULT CURRENT_DATE,
     category_id INTEGER NOT NULL,
-    FOREIGN KEY (category_id) REFERENCES category(id)
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES category(id),
+    FOREIGN KEY (user_id) REFERENCES user_account(id)
 );
+
+-- Wstawianie domyślnych wartości do tabeli ról
+INSERT INTO role (name) VALUES ('ADMIN'), ('LIMITED_USER'), ('FULL_USER'), ('GUEST');
